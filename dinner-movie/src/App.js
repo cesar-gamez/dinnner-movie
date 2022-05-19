f8551b770e4ac76323d4ea12740664ebae0371c8
Cesar Gamez <w89282@gamezleal.com>
2022-05-19 17:26:46 -0500

    adding initial React app files

---
+++ dinner-movie/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+function App() {
+    const[name, setName] = useState([]);
+    const[img, setImg] = useState([]);
+    const apiKey = "bd5ddc60f5de8d9d668eb738d1c542aa"
+
+    function fetchMovie() {
+        var movieId = Math.floor(Math.random()*763741);
+
+        function fetchRetry(tries) {
+            movieId = Math.floor(Math.random()*763741);
+            return new Promise(function(resolve, reject) {
+                fetch(`https://api.themoviedb.org/3/movie/${String(movieId)}?api_key=${apiKey}&language=en-US`).then(resolve)
+                    .catch(function(error) {
+                        if(tries == 1) return reject(error);
+                        fetchRetry(tries-1)
+                            .then(resolve)
+                            .catch(reject);
+                    })
+            });
+        }
+
+        fetch(`https://api.themoviedb.org/3/movie/${String(movieId)}?api_key=${apiKey}&language=en-US`)
+            .then(res => {
+                if(res.ok)
+                    return res.json()
+            })
+            .then((result) => {
+                setName(result.original_title)
+                setImg(result.poster_path)
+                console.log(movieId)
+            })
+            .catch((error) => {
+                fetchRetry(10)
+            })
+    }
+
+    return (
+        <div style={{
+        display: 'flex', flexDirection: 'column', alignItems: 'center'
+        }}>
+            <h2 style={{
+            position: 'absolute', left: '2%', top: '1%',
+            }}>Dinner & Movie</h2>
+            <div style={{alignContent: 'center'}
+            >
+                <img src="http://api.themoviedb.org/3/movie/142808/images?api_key=bd5ddc60f5de8d9d668eb738d1c542aa"/>
+                <h1>{name}</h1>
+                <button onClick={fetchMovie} style={{
+                background: '#f8465d', borderRadius: '100px', color: 'white', border: '0',
+                width: '150px', height: '40px'
+                }}>Fetch</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
